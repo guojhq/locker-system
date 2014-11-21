3a09ee959528d31f3e532d879160bb4ef1202864
Keiko Climaco <keikoclimaco@gmail.com>
2014-11-21 03:36:34 -0500

    basic css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'locker_system/index'
+
+  resources :articles
+
+  #get 'locker_system/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
