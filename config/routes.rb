d690437764998741dd7c5148e6cf433189314d0b
Keiko Climaco <keikoclimaco@gmail.com>
2014-11-23 13:58:44 -0500

    finished scaffold generations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :lockers
+
   resources :registrations
 
   resources :students
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141123185408) do
+ActiveRecord::Schema.define(version: 20141123185734) do
+
+  create_table "lockers", force: true do |t|
+    t.integer  "lockerNumber"
+    t.string   "location"
+    t.string   "type"
+    t.integer  "combo"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "organizations", force: true do |t|
     t.string   "name"
