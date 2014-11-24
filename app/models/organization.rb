b11aa8aed72b28a834fb23fee4105bbd65b0bb57
Keiko Climaco <keikoclimaco@gmail.com>
2014-11-24 01:17:20 -0500

    postalcode issues trying to resolve

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,7 @@
 class Organization < ActiveRecord::Base
 	has_many :students
 	has_many :lockers
-	validates :postalcode, length: { maximum: 6 }, presence: true
+	validates :postalCode, length: { maximum: 6 }, presence: true
 	validates :phone, length: { maximum: 10 }, presence: true
 	validates :streetAddress, presence: true
 	validates :city, presence: true
