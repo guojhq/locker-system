53e1f260add5a957ddbc7762e4fcf77102feb618
Keiko Climaco <keikoclimaco@gmail.com>
2014-11-24 11:51:12 -0500

    locker validation

--- app/models/locker.rb
+++ app/models/locker.rb
@@ -1,2 +1,8 @@
 class Locker < ActiveRecord::Base
+	belongs_to :registration
+	belongs_to :organization
+	validates :lockerNumber, presence: true
+	validates :location, presence: true
+	validates :type, presence: true
+	validates :combo, presence: true
 end
