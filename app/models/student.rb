911751e36037ebc651c86ea62bb0930a300804c6
Keiko Climaco <keikoclimaco@gmail.com>
2014-11-24 02:58:14 -0500

    finished students scaffold, reset registration

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,2 @@
 class Student < ActiveRecord::Base
-	has_one :registration
-	belongs_to :organization
-	validates :studentNumber, length: { maximum: 7 }, presence: true
-	validates :firstName, presence: true
-	validates :lastName, presence: true
-	validates :email, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :students
+
   resources :lockers
 
-
-  resources :registrations
 
   resources :organizations
 
