0b18ac7461d7e407f8af649c28300317434f5d62
Keiko Climaco <keikoclimaco@gmail.com>
2014-11-23 21:15:39 -0500

    uploading lockers class

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124021003) do
+ActiveRecord::Schema.define(version: 20141124021434) do
+
+  create_table "lockers", force: true do |t|
+    t.integer  "lockerNumber"
+    t.string   "location"
+    t.string   "type"
+    t.integer  "combo"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "organizations", force: true do |t|
     t.string   "name"
