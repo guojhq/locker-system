60757b94befe0701d312a6bc7f5125ec8b33c27f
Keiko Climaco <keikoclimaco@gmail.com>
2014-11-24 16:31:26 -0500

    confirmation

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,14 +1,14 @@
 class Registration < ActiveRecord::Base
 	has_many :lockers
 	belongs_to :organization
-	validates :studentNumber, presence: true
+	validates :studentNumber, presence: true, length: {maximum: 7}
 	validates :firstName, presence: true
 	validates :lastName, presence: true
 	validates :lockerNumber, presence: true
 	validates :lockerType, presence: true
 	validates :lockerCombo, presence: true
 	validates :numberOfSemesters, presence: true
-	validates :termsAccepted, presence: true
+	validates :termsAccepted, presence: true, confirmation: true
 
 	def self.search(query)
 	  where("lockerNumber like ?", "%#{query}%")
