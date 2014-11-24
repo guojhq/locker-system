700e74c359aaebbadcf6283482b7c27a19654fa0
Keiko Climaco <keikoclimaco@gmail.com>
2014-11-24 01:02:46 -0500

    trying to fix organizations error

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -69,6 +69,6 @@ class OrganizationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def organization_params
-      params.require(:organization).permit(:name, :streetAddress, :city, :country, :postalCode, :phone, :email)
+      params.require(:organization).permit(:name, :streetAddress, :city, :country, :postalcode, :phone, :email)
     end
 end
