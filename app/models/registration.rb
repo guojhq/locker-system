d31ed0e9a72fcab5bd766e4f2edf409239f591b8
Keiko Climaco <keikoclimaco@gmail.com>
2014-11-24 16:53:46 -0500

    locker type error check is not working

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -6,16 +6,17 @@ class Registration < ActiveRecord::Base
 	validates :lastName, presence: true
 	validates :email, presence: true
 	validates :lockerNumber, presence: true
-	validates :lockerType, presence: true, :check_locker_type
+	validates :lockerType, presence: true, 
 	validates :lockerCombo, presence: true
 	validates :numberOfSemesters, presence: true
 	validates :termsAccepted, presence: true, confirmation: true
+	validates :check_locker_type
 
 	def self.search(query)
 	  where("lockerNumber like ?", "%#{query}%")
 	end
 
 	def check_locker_type
-		errors.add(:lockerType, "Must be keyed or Coded") if lockerType != "Keyed" or lockerType != "Coded"
+		errors.add(:lockerType, "Must be keyed or Coded") if self.lockerType != "Keyed" or lockerType != "Coded"
 	end
 end
