70276e3f6c1f9642228ea7e0d3b7307437b8462e
Keiko Climaco <keikoclimaco@gmail.com>
2014-11-24 11:34:54 -0500

    registrations validation

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,11 +1,12 @@
 class Organization < ActiveRecord::Base
 	#SEG2105 Keiko Climaco 5999930
 	#Setting up associations
-	has_many :students
+	has_many :registrations
 	has_many :lockers
 	validates :name, presence: true
 	validates :postalCode, length: { maximum: 6 }, presence: true
 	validates :phone, length: { maximum: 10 }, presence: true
+	validates :country, presence: true
 	validates :streetAddress, presence: true
 	validates :city, presence: true
 	validates :email, presence: true
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,2 +1,12 @@
 class Registration < ActiveRecord::Base
+	has_many :lockers
+	belongs_to :organization
+	validates :studentNumber, presence: true
+	validates :firstName, presence: true
+	validates :lastName, presence: true
+	validates :lockerNumber, presence: true
+	validates :lockerType, presence: true
+	validates :lockerCombo, presence: true
+	validates :numberOfSemesters, presence: true
+	validates :termsAccepted, presence: true
 end
