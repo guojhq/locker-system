443ded14e8888550ee75ca08536dd4a9288ef8f8
Keiko Climaco <keikoclimaco@gmail.com>
2014-11-24 02:30:37 -0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :students
 
-  resources :organizations, param: :name
+  resources :organizations
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
