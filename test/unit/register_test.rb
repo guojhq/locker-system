861a21ce9b25fcc2c655a1441117fc85c83e23ff
Nick Burgel <nickburgel@me.com>
2014-12-01 12:10:59 -0500

    blah blah blah

--- test/unit/register_test.rb
+++ test/unit/register_test.rb
@@ -3,12 +3,6 @@ require 'test_helper'
 class RegisterTest < ActiveSupport::TestCase
   
   test "add orginaization" do
-    org = Organization.new
-    assert true
-  end
-  
-  
-  test "create registration" do
     assert true
   end
 end
