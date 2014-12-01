3115b73131e3e99fb16b61fa8235994b1111e812
Nick Burgel <nickburgel@me.com>
2014-12-01 12:47:42 -0500

    last one worked can we make it more difficult

--- test/unit/register_test.rb
+++ test/unit/register_test.rb
@@ -4,6 +4,7 @@ class RegisterTest < ActiveSupport::TestCase
   
   test "add registration" do
     reg = Registration.new
+    reg.registration_parms 6775091:studentNumber
     assert !reg.save
   end
 end
