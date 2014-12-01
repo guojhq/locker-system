76b4b6ee0c6f503be12b67b5e5ee07bdf86f6276
Keiko Climaco <keikoclimaco@gmail.com>
2014-12-01 13:20:57 -0500

    fixing registration validations

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,14 +3,14 @@ class Registration < ActiveRecord::Base
 	# Keiko Climaco 5999930, validation checks for user input
 	has_many :lockers
 	belongs_to :organization
-	validates :studentNumber, presence: true, length: {maximum: 7}
+	validates :studentNumber, presence: true, :length => {maximum: 7}, :numericality => {:only_integer => true}
 	validates :firstName, presence: true
 	validates :lastName, presence: true
 	validates :email, presence: true
-	validates :lockerNumber, presence: true
+	validates :lockerNumber, presence: true, :numericality => {:only_integer => true}
 	validates :lockerType, presence: true, inclusion: {in: %w(keyed coded), message: "%{value} is not a valid locker type"} 
-	validates :lockerCombo, presence: true
-	validates :numberOfSemesters, presence: true
+	validates :lockerCombo, presence: true, :length => {maximum: 4}, :numericality => {:only_integer => true}
+	validates :numberOfSemesters, presence: true, :length => {maximum: 1}, :numericality => {:only_integer => true}
 	validates :termsAccepted, presence: true, confirmation: true
 
 	# Keiko Climaco 5999930, implementing search function
