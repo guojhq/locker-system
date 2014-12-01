b538c9fd1bb7cd4f80d94370cce7aa428fe79c1d
Nick Burgel <nickburgel@me.com>
2014-12-01 14:41:23 -0500

    IDK anymore

--- test/unit/registration_test.rb
+++ test/unit/registration_test.rb
@@ -1,10 +1,9 @@
 require File.expand_path('test/test_helper')
  
-class RegisterTest < ActiveSupport::TestCase
+class RegistrationTest < ActiveSupport::TestCase
   
   test "add registration" do
     reg = Registration.new 
-    #reg :studentNumber => 6775091, :firstName => 'Nick'
     assert !reg.save
   end
   
