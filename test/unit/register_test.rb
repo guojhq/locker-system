74a95a74e089433434c8e9c9359af09c7ed38dc7
Nick Burgel <nickburgel@me.com>
2014-12-01 13:04:32 -0500

    I really going into left field now

--- test/unit/register_test.rb
+++ test/unit/register_test.rb
@@ -3,7 +3,7 @@ require File.expand_path('test/test_helper')
 class RegisterTest < ActiveSupport::TestCase
   
   test "add registration" do
-    reg = Registration.new(6775091 :studentNumber, Nick :firstName, Burgel :lastName)
+    registration :create, :Registraon => { :studentNumber => 6775091, :firstName => 'Nick'}
     assert !reg.save
   end
 end
