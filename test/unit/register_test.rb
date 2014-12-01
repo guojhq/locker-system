9a0b188d8f19bd37a952eb4c6586c7e6e7cce877
Nick Burgel <nickburgel@me.com>
2014-12-01 13:06:50 -0500

    oh yea

--- test/unit/register_test.rb
+++ test/unit/register_test.rb
@@ -3,7 +3,8 @@ require File.expand_path('test/test_helper')
 class RegisterTest < ActiveSupport::TestCase
   
   test "add registration" do
-    Registration :create, :Registraon => { :studentNumber => 6775091, :firstName => 'Nick'}
+    reg = Registration.new 
+    reg :create, :Registraon => { :studentNumber => 6775091, :firstName => 'Nick'}
     assert !reg.save
   end
 end
