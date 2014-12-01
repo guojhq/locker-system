c367d1889d4778205d44ba9cb8846dda1c5a91d9
Keiko Climaco <keikoclimaco@gmail.com>
2014-12-01 13:32:01 -0500

    more validations!

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,18 +3,22 @@ class Registration < ActiveRecord::Base
 	# Keiko Climaco 5999930, validation checks for user input
 	has_many :lockers
 	belongs_to :organization
-	validates :studentNumber, presence: true, :length => {maximum: 7}, :numericality => {:only_integer => true}
+	validates :studentNumber, presence: true, :length => {:is => 7}, :numericality => {:only_integer => true}
 	validates :firstName, presence: true
 	validates :lastName, presence: true
 	validates :email, presence: true
-	validates :lockerNumber, presence: true, :numericality => {:only_integer => true}
+	validates :lockerNumber, presence: true, :numericality => {:only_integer => true}, :uniqueness => true
 	validates :lockerType, presence: true, inclusion: {in: %w(keyed coded), message: "%{value} is not a valid locker type"} 
-	validates :lockerCombo, presence: true, :length => {maximum: 4}, :numericality => {:only_integer => true}
-	validates :numberOfSemesters, presence: true, :length => {maximum: 1}, :numericality => {:only_integer => true}
+	validates :lockerCombo, presence: true, :length => {:is => 4}, :numericality => {:only_integer => true}
+	validates :numberOfSemesters, presence: true, :length => {:is => 1}, :numericality => {:only_integer => true}, :if => :valid_number?
 	validates :termsAccepted, presence: true, confirmation: true
 
 	# Keiko Climaco 5999930, implementing search function
 	def self.search(query)
 	  where("lockerNumber like ?", "%#{query}%")
+	end
+
+	def valid_number?
+		numberOfSemesters == 1 || numberOfSemesters == 2
 	end
 end
